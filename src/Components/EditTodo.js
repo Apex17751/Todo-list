45bd1cf50ac6ff8a6a9202262a61fee274207c38
Praise Afolabi <praizafolabi12@gmail.com>
2023-05-17 12:53:40 +0100

    recommitting

--- src/Components/EditTodo.js
+++ src/Components/EditTodo.js
@@ -12,10 +12,10 @@ const EditTodo = ({editButton, task}) => {
     <div>
           <form>
          <div className="flex mx-auto items-center justify-center mb-6 mt-4 relative" id='edit'>
-         <input type="text" name="" className='rounded-lg py-2 px-3 flex outline-none' placeholder='Update Task...'value={value} onChange={(e) => setValue(e.target.value)} />
-          <div className='absolute right-24'>
+         <input type="text" name="" className='rounded-lg py-2 px-3 flex outline-none' id='update' placeholder='Update Task...'value={value} onChange={(e) => setValue(e.target.value)} />
+          <div className='absolute right-0'>
           <button type='button' className
-          =' flex px-3 py-2  rounded-md bg-orange-400 text-white uppercase text-center' onClick={handleSubmit}>Update</button>
+          =' flex px-3 py-2  rounded-md bg-orange-400 text-white uppercase text-center' id='btnz' onClick={handleSubmit}>Update</button>
           </div>
          </div>
           </form>      
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -7,12 +7,12 @@ const TodoList = ({task, toggleComplete, deleteButton, editButton}) => {
   return (
     <div className='mt-4'>
         <div className='flex px-3 py-3  rounded-lg bg-orange-400 text-white text-center lg:w-72' id='btn'>
-            <div className='flex items-center pl-3 justify-between cursor-pointer'>
+            <div className='flex items-center sm:pl-3 justify-between cursor-pointer'>
             <h1 onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ''}`}>{task.task}</h1>
             
             <div>
             <FontAwesomeIcon icon={faPenToSquare} onClick={() => editButton(task.id)} className='cursor-pointer' />
-            <FontAwesomeIcon icon={faTrash} onClick={()=> deleteButton(task.id)} className='pl-3 pr-2 cursor-pointer' />
+            <FontAwesomeIcon icon={faTrash} onClick={()=> deleteButton(task.id)} id='trash' className='sm:pl-3 sm:pr-2 cursor-pointer' />
             </div>
             </div>
         </div>
